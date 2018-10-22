9b8a888f922c9e4deb2493beefbaa9b7ff1c7269
linchengzzz <1139489076@qq.com>
2018-10-22 17:06:37 +0800

    python str

--- 06-10/list.py
+++ 06-10/list.py
@@ -1,8 +1,119 @@
+# def main():
+#     list1 = [1, 3, 5, 7, 100]
+#     print(list1)
+#     list2 = ['hello'] * 5
+#     print(list2)
+#     print(len(list1))
+#     print(list1[0])
+#     print(list1[4])
+#     # print(list1[5])
+#     print(list1[-1])
+#     print(list1[-3])
+#     list1[2] = 300
+#     print(list1)
+#     list1.append(200)
+#     print(list1)
+#     list1.insert(1, 400)
+#     print(list1)
+#     list1 += [1000, 2000]
+#     print(list1)
+#     print(len(list1))
+#     list1.remove(300)
+#     print(list1)
+#     if 1234 in list1:
+#         list1.remove(1234)
+#     del list1[0]
+#     print(list1)
+#     list1.clear()
+#     print(list1)
+
+
+# def main():
+#     fruits = ['grape', 'apple', 'strawberry', 'wa']
+#     fruits += ['hi', 'hello', 'mango']
+#     for fruit in fruits:
+#         print(fruit.title(), end=' ')
+#     print()
+#     fruits2 = fruits[1:4]
+#     fruits3 = fruits[:]
+#     fruits4 = fruits
+#     print(fruits3, fruits4)
+#     print(fruits2)
+#     fruits3[3] = '111'
+#     fruits4[3] = '222'
+#     print(fruits3, fruits4, fruits)
+#     fruits5 = fruits[-3:-1]
+#     fruits6 = fruits[::-1]
+#     print(fruits5, fruits6)
+
+
+# def f(x):
+#     return len(x)
+#
+#
+# def main():
+#     list1 = ['orange', 'apple', 'zoo', 'inter', 'blueberry']
+#     list2 = sorted(list1)
+#     print(list1, list2)
+#     list3 = sorted(list1, reverse=True)
+#     list4 = sorted(list2, key=f)
+#     print(list3, list4)
+#     list1.sort(reverse=True)
+#     print(list1)
+
+# import sys
+#
+#
+# def main():
+#     f = [x for x in range(1, 10)]
+#     print(f)
+#     f = [x + y for x in 'ABCDE' for y in '1234567']
+#     print(f)
+#     # f = [x ** 2 for x in range(1, 1000)]
+#     # print(f)
+#     # print(len(f))
+#     # print(sys.getsizeof(f))
+#     for val in f:
+#         print(val)
+
+
+# def fib(n):
+#     a, b = 0, 1
+#     for _ in range(n):
+#         a, b = b, a + b
+#         yield a
+#
+#
+# def main():
+#     # for val in fib(20):
+#     #     print(val)
+#     f = fib(20)
+#     print(next(f))
+#     print(next(f))
+#     print(next(f))
+#     print(next(f))
+#     print(next(f))
+#     print(next(f))
+
+
 def main():
-    list1 = [1, 3, 5, 7, 100]
-    print(list1)
-    list2 = ['hello'] * 5
-    print(list2)
+    t = ('zcl', 38, True, 'Beijing')
+    print(t)
+    print(t[0])
+    print(t[3])
+    for member in t:
+        print(member)
+    # t[0] = '111'
+    t = ('11', 22, True, 'Beijing')
+    print(t)
+    person = list(t)
+    print(person)
+    person[0] = '1122'
+    person[1] = 24
+    print(person)
+    fruits = ['apple', 'banana', 'orange']
+    fruits_tuple = tuple(fruits)
+    print(fruits_tuple)
 
 
 if __name__ == '__main__':
--- 06-10/str.py
+++ 06-10/str.py
@@ -1,31 +1,88 @@
 
+# def main():
+#     str1 = 'hello'
+#     print(str1)
+#     print(len(str1))
+#     print(str1.capitalize())
+#     print(str1.upper())
+#     print(str1.find('lo'))
+#     print(str1.find('sh'))
+#     # print(str1.index('lo'))
+#     # print(str1.index('sh'))
+#     print(str1.startswith('he'))
+#     print(str1.startswith('He'))
+#     print(str1.center(20, '*'))
+#     print(str1.rjust(50, ' '))
+#     str2 = 'abc123456'
+#     print(str2[2])
+#     print(str2[2:5])
+#     print(str2[2:])
+#     print(str2[::2])
+#     print(str2[::-1])
+#     print(str2.isdigit())
+#     print(str2.isalpha())
+#     print(str2.isalnum())
+#     str3 = '  jackfrued@126.com '
+#     print(str3)
+#     # 获得字符串修剪左右两侧空格的拷贝
+#     print(str3.strip())
+
+
+# def main():
+#     set1 = {1, 2, 3, 3, 3, 2}
+#     print(set1)
+#     print('Length = ', len(set1))
+#     set2 = set(range(1,10))
+#     print(set2)
+#     set1.add(4)
+#     set1.add(5)
+#     set2.update([11,12])
+#     print(set1)
+#     print(set2)
+#     set2.discard(5)
+#     print(set2)
+#     set2.discard(5)
+#     print(set2)
+#     set2.remove(4)
+#     print(set2)
+#     # set2.remove(4)
+#     # print(set2)
+#     # for elem in set2:
+#     #     print(elem ** 2, end=' ')
+#     # print()
+#     set3 = set((1,2,3,3,2,1))
+#     # print(set3)
+#     # print(set3.pop())
+#     # print(set3)
+#     print('&: ', set1 & set2)
+#     print('-: ', set1 - set2)
+#     print('|: ', set1 | set2)
+#     print('^: ', set1 ^ set2)
+#     print('<=: ', set2 <= set1)
+#     print('<=: ', set3 <= set1)
+#     print('>=: ', set1 >= set2)
+#     print('>=: ', set1 >= set3)
+
+
 def main():
-    str1 = 'hello'
-    print(str1)
-    print(len(str1))
-    print(str1.capitalize())
-    print(str1.upper())
-    print(str1.find('lo'))
-    print(str1.find('sh'))
-    # print(str1.index('lo'))
-    # print(str1.index('sh'))
-    print(str1.startswith('he'))
-    print(str1.startswith('He'))
-    print(str1.center(20, '*'))
-    print(str1.rjust(50, ' '))
-    str2 = 'abc123456'
-    print(str2[2])
-    print(str2[2:5])
-    print(str2[2:])
-    print(str2[::2])
-    print(str2[::-1])
-    print(str2.isdigit())
-    print(str2.isalpha())
-    print(str2.isalnum())
-    str3 = '  jackfrued@126.com '
-    print(str3)
-    # 获得字符串修剪左右两侧空格的拷贝
-    print(str3.strip())
+    scores = {'111': 10, '222': 20, '333': 30}
+    print(scores['111'])
+    print(scores['222'])
+    for elem in scores:
+        print('%s\t--->\t%d' % (elem, scores[elem]))
+    scores['111'] = 60
+    scores['222'] = 70
+    scores.update(你好=30, 哈哈=50)
+    print(scores)
+    # print(scores['4444'])
+    print(scores.get('444', 30))
+    print(scores.get('111', 40))
+    print(scores)
+    print(scores.popitem())
+    print(scores.popitem())
+    print(scores.pop('111', 3333))
+    scores.clear()
+    print(scores)
 
 
 if __name__ == '__main__':
